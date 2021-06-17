1edd5327a38cf38994ce7650cfd34fcbee36d0be
calvinraveenthran <calvin.raveenthran@gmail.com>
2021-06-16 21:33:39 -0400

    Short

--- src/vars-and-const/vars.go
+++ src/vars-and-const/vars.go
@@ -5,13 +5,13 @@ import (
 	"reflect"
 )
 
-var (
-	name   string
-	course string
-	module float64
-)
-
 func main() {
+
+	name := "Nigel"
+	course := "Docker Deep Dive"
+	module := "3.2"
+
 	fmt.Println("Name is set to:", name, "and is of type ", reflect.TypeOf(name))
-	fmt.Println("Modile is set to:", module, "and is of type ", reflect.TypeOf(module))
+	fmt.Println("Module is set to:", module, "and is of type ", reflect.TypeOf(module))
+	fmt.Println("Course is set to:", course, "and is of type ", reflect.TypeOf(course))
 }

01f5397778f4a6cd079e165c6526e19c1cc6bed0
calvinraveenthran <calvin.raveenthran@gmail.com>
2021-06-17 23:20:52 -0400

    Simple if statement

---
+++ src/conditionals/first-if.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	firstRank := 1
+	secondRank := 2
+
+	if firstRank < secondRank {
+		fmt.Println("\nFirst course is doing better than the second course")
+	}
+}

211c412d8de9428fd2cf28ada2e7751a05450fdb
calvinraveenthran <calvin.raveenthran@gmail.com>
2021-06-17 23:23:32 -0400

    Some mods

--- src/conditionals/first-if.go
+++ src/conditionals/first-if.go
@@ -6,10 +6,14 @@ import (
 
 func main() {
 
-	firstRank := 1
+	firstRank := 2
 	secondRank := 2
 
 	if firstRank < secondRank {
 		fmt.Println("\nFirst course is doing better than the second course")
+	} else if secondRank < firstRank {
+		fmt.Println("\nSecond course is doing better than the first course")
+	} else {
+		fmt.Println("\nThey have the same rank")
 	}
 }

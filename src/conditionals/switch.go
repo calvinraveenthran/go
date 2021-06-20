3e7a9ca4f9357000b70aa9101f72a9c12610ce67
calvinraveenthran <calvin.raveenthran@gmail.com>
2021-06-20 09:37:58 -0400

    Trying fallthroughs

--- src/conditionals/switch.go
+++ src/conditionals/switch.go
@@ -7,12 +7,15 @@ import (
 func main() {
 
 	switch "docker" {
-	case "linux":
-		fmt.Println("\nHere are some linux courses")
 	case "docker":
 		fmt.Println("\nHere are some docker courses")
+		fallthrough //prints the guy below it.
+	case "linux":
+		fmt.Println("\nHere are some linux courses")
+		fallthrough
 	case "windows":
 		fmt.Println("\nHere are some window courses")
+		fallthrough
 	default:
 		fmt.Println("\nSorry, we couldn't find a match!")
 	}
